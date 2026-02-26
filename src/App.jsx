b3bab4179079c5c622e31c9aac0e2116493f6808
qwerewqwerew <qwerew0@gmail.com>
2026-02-26 16:37:09 +0900

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,6 @@ export default function App() {
   return (
     <>
       <Nav />
-      <main>
-        <section id="home">
-          <h1>Home</h1>
-        </section>
-        <section id="about">
-          <h2>About</h2>
-        </section>
-        <section id="projects">
-          <h2>Projects</h2>
-        </section>
-        <section id="contact">
-          <h2>Contact</h2>
-        </section>
-      </main>
     </>
   );
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,14 +2,11 @@ import React from "react";
 
 export function Nav() {
   return (
+    // nav>.logo+.list
     <nav>
-      <div className="logo">My Resume</div>
-      <ul className="list">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+      <div className="logo"></div>
+      <div className="list">
+      </div>
     </nav>
   );
 }
