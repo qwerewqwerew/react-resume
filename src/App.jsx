ee5cb23b7c6aeee4c9e589681d8295e9811cfe3e
qwerewqwerew <qwerew0@gmail.com>
2026-02-27 16:37:55 +0900

    feat: 심플 네비게이션 및 기본 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { Nav } from "./Nav";
+import { Nav } from "./components/Nav";
+import { AboutMe } from "./components/AboutMe";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { Projects } from "./components/Projects";
 
 export default function App() {
   return (
-    <>
+    <div className="app container">
       <Nav />
-    </>
+      <Hero />
+      <main>
+        <AboutMe />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+export const Nav = () => {
+  return (
+    <header>
+      <nav className="nav simple-nav">
+        <div className="logo">
+          <img src="logo.png" alt="" />
+        </div>
+        <div className="menu">
+          <div className="home">home</div>
+          <div className="about">about</div>
+          <div className="projects">projects</div>
+        </div>
+      </nav>
+    </header>
+  );
+};
